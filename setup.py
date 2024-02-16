85b0b6335e5ded6f1b886da732305a3a942a565d
ToroHack <torohack.github@gmail.com>
2024-02-16 22:08:13 +0100

    import statements

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
+import setuptools
 from pathlib import Path
-from setuptools import find_namespace_packages, setup
 
 # Load packages from requirements.txt
 BASE_DIR = Path(__file__).parent
@@ -9,9 +9,17 @@ with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
 setuptools.setup(
     name="ML model to cloud",
     version="0.0.0",
+    author_email="ricardraigada@outlook.es",
     description="Deploying a ML model to cloud application platform\
         with FastAPI",
     author="Ricard Santiago Raigada García",
-    packages=find_namespace_packages(),
+    url="https://github.com/ToroData/Deploying-a-ML-Model-to-Cloud-Application-Platform-with-FastAPI",
+    packages=setuptools.find_namespace_packages(),
     install_requires=[required_packages],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8'
 )
